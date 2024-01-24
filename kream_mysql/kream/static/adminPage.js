9b14d82373da7cae6682c2d26562e9a898a86747
Meoyoug <eodyd426@gmail.com>
2024-01-24 17:59:38 +0900

    3차 중간 수정본

--- kream_mysql/kream/static/adminPage.js
+++ kream_mysql/kream/static/adminPage.js
@@ -3,7 +3,7 @@ function delete_product(){
     var selected_rows=[];
     checked_checkbox.forEach(function(checkbox){
       var row = checkbox.closest('tr');
-      var product = row.querySelector('td:nth-child(4)').textContent;
+      var product = row.querySelector('h5').textContent;
       console.log(product)
       selected_rows.push(product)
     })
@@ -38,8 +38,11 @@ function update_product(){
   })
   .then(response => response.json())
   .then(data => {
-    if (!data.success){
+    if (data.success){
+      window.location.href = "https://127.0.0.1:5000/";
+    } else {
       alert('업데이트 중 오류 발생')
+
     }
   })
 }
